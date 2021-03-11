505d89552b390582a118b8d6990b3da913692ae2
Stijn Rogiest <reddusted@gmail.com>
2021-03-11 21:13:00 +0100

    Use commander.js for cli

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,30 @@
-import ts from "byots";
+import ts, { programContainsEs6Modules } from "byots";
 import fs from "fs";
 import path from "path";
 import { getFromSourceFile, generatePackageContent, Validators, Methods } from "./generator";
+import { Command } from "commander";
 
 function main() {
-    console.log("Starting...");
+    let program = new Command("route-gen");
+    program.version("1.0.0");
+    program.requiredOption("-i --input <file>", "The .ts file containing Routes/Validation namespaces.");
+    program.option("-o --output <file>", "The destination file to generate. Will be overwritten.", "generatedClient.ts");
+    program.parse(process.argv);
+    let options = program.opts();
+    let inputFile = options.input;
+    let outputFile = options.output;
+    let inputFileDir = path.dirname(inputFile);
+    console.log(`${inputFile} -> ${outputFile}`);
 
-    let routeDefinitionsPath = process.argv[2]; // .slice(2).join(" ")
-    if (!routeDefinitionsPath) {
-        throw new Error("Invalid usage, please specify the files containing your routes");
-    }
-    console.log("Reading routes from ", routeDefinitionsPath);
-
-    let configFileName = ts.findConfigFile(routeDefinitionsPath, ts.sys.fileExists, "tsconfig.json");
+    let configFileName = ts.findConfigFile(process.cwd(), ts.sys.fileExists, "tsconfig.json");
     if (!configFileName) {
         throw new Error("tsconfig.json could not be found");
     }
     let compilerOptionsFile = ts.readConfigFile(configFileName, ts.sys.readFile);
     let compilerOptions = ts.parseJsonConfigFileContent(compilerOptionsFile.config, ts.sys, "./").options;
-
-    let program = ts.createProgram([routeDefinitionsPath], compilerOptions);
+    let typescriptProgram = ts.createProgram([inputFile], compilerOptions);
+    let inputSourceFile = typescriptProgram.getSourceFile(inputFile)!;
+    if (!inputSourceFile) throw new Error(`Input file '${inputFile}' does not exist.`);
 
     let validatorTypes: Validators = {};
     let methodTypes: Methods = {
@@ -32,12 +37,10 @@ function main() {
         head: {},
         all: {},
     };
-    // files.forEach((file) => generateFromSourceFile(program, program.getSourceFile(file)!, types, methodTypes, validatorTypes));
-    let sourceFile = program.getSourceFile(routeDefinitionsPath)!;
-    getFromSourceFile(program, sourceFile, methodTypes, validatorTypes);
+    getFromSourceFile(typescriptProgram, inputSourceFile, methodTypes, validatorTypes);
 
-    let output = fs.createWriteStream(path.join(path.dirname(routeDefinitionsPath), "generatedClient.ts"));
-    generatePackageContent(program.getTypeChecker(), validatorTypes, methodTypes, output, path.dirname(routeDefinitionsPath));
+    let output = fs.createWriteStream(outputFile); // path.join(inputFileDir, outputFile)
+    generatePackageContent(typescriptProgram.getTypeChecker(), validatorTypes, methodTypes, output, inputFileDir);
     output.close();
 }
 
