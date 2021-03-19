457eb51a69b0904c346a8e686f54300bff5efab5
Stijn Rogiest <reddusted@gmail.com>
2021-03-19 19:48:37 +0100

    v1.1.3

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import { generateCode, getRouteTypes, getRouteTypesFromRoutesNamespace, PathType
 import { Command } from "commander";
 import chokidar from "chokidar";
 
-const VERSION = "1.1.2";
+const VERSION = "1.1.3";
 
 function main() {
     let program = new Command("typed-rest-generator");
